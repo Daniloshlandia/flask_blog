5b60fcad25cd1e059f921ebc24dcf51ab8908507
Danilo Altamirano <danilosh45@gmail.com>
2020-04-11 00:29:47 +0200

    bug about no callable app on wsgi file resolve

--- wsgi.py
+++ wsgi.py
@@ -3,5 +3,5 @@ from webapp import create_app
 from webapp.cli import register
 
 env = os.environ.get('WEBAPP_ENV', 'dev')
-application = create_app('config.%sConfig' % env.capitalize())
-register(application)
+app = create_app('config.%sConfig' % env.capitalize())
+register(app)
