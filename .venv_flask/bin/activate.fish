ad5c6ee83b7e958e7c9a708b2de0c3420e713b28
danilosh45 <danilosh45@gmail.com>
2020-04-15 20:14:56 +0200

    resolving bugs fix and others problem to deploy

--- .venv_flask/bin/activate.fish
+++ .venv_flask/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/dash/src/flask/flask_blog/.venv_flask"
+set -gx VIRTUAL_ENV "/home/dash/src/flask_blog/.venv_flask"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
